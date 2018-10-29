27c2f5ca342c86fe1b41f36a1da7c2b67651cedb
Angel Garcia <angelgarcia@fbacks.com>
2018-10-29 16:53:14 -0400

    Back to original state

--- android/src/main/java/com/shahenlibrary/Trimmer/Trimmer.java
+++ android/src/main/java/com/shahenlibrary/Trimmer/Trimmer.java
@@ -497,8 +497,6 @@ public class Trimmer {
     cmd.add(source);
     cmd.add("-c:v");
     cmd.add("libx264");
-    cmd.add("-c:a");
-    cmd.add("copy");
     cmd.add("-profile:v");
     cmd.add("baseline");
     cmd.add("-level");
