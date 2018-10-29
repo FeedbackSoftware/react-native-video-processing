1012526984115c9a651e8c55e6a86dda8ea2f577
Angel Garcia <angelgarcia@fbacks.com>
2018-10-29 16:11:03 -0400

    Removes zero latency option for android compression

--- android/src/main/java/com/shahenlibrary/Trimmer/Trimmer.java
+++ android/src/main/java/com/shahenlibrary/Trimmer/Trimmer.java
@@ -496,7 +496,7 @@ public class Trimmer {
     cmd.add("-i");
     cmd.add(source);
     cmd.add("-c:v");
-    cmd.add("copy");
+    cmd.add("libx264");
     cmd.add("-c:a");
     cmd.add("copy");
     cmd.add("-profile:v");
